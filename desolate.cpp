6e5c81e7072e610b6084471da6426c6bed7d7c99
Steve V <Steven.D.Vaught@gmail.com>
2016-12-20 19:18:53 -0600

    create basic cpp

---
+++ desolate.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <iomanip>
+#include <list>
+#include <forward_list>
+#include <vector>
+#include <sstream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+  cout << "Hello world"<<endl;
+  return 0;
+}
+
+
