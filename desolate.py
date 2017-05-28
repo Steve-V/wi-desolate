f528d6db47b0aff6c6fb573a890dffd67cbd5d9e
Steve Vaught <Steven.D.Vaught@gmail.com>
2017-05-28 13:34:59 -0500

    rm comments, comment out long-lat functionality

--- desolate.py
+++ desolate.py
@@ -6,7 +6,7 @@ from geopy.distance import great_circle, vincenty
 from tqdm import tqdm
 import numpy, itertools
 
-def allWisconsinPoints(latlong=True, shortList=False):
+def allWisconsinPoints(shortList=False):
     '''Return every possible latitude and longitude coordinate occurring in Wisconsin's bounding box.
     Ordinarily returns lat/long, but passing reversed=True returns long/lat
     Passing shortList only returns a handful of points, for testing purposes
@@ -18,35 +18,19 @@ def allWisconsinPoints(latlong=True, shortList=False):
 
     wisLatitudes = numpy.arange(42,44,latstep)
     wisLongitudes = numpy.arange(-87,-90,lonstep)
-    return itertools.product(wisLatitudes,wisLongitudes) if latlong else itertools.product(wisLongitudes,wisLatitudes)
+    return itertools.product(wisLatitudes,wisLongitudes) 
+    # return tertools.product(wisLongitudes,wisLatitudes) # if for some reason we ever want to use long-lat ordering
 
 def distanceToNearestAirport(point, allAirports):
     '''Given a point, determine the distance to the nearest airport'''
     return min( [getDistance(point, eachAirport) for eachAirport in allAirports] )
 
 def getDistance(point, airport):
-    # print(point, airport,vincenty(point,airport).nautical)
     return great_circle(point,airport).nautical
 
 def main():
-    # myButt = allWisconsinAirportsList(True)
-    # reversedButt = allWisconsinAirportsList(False)
-    # print(myButt[4])
-    # print(reversedButt[4])
-    # print(myButt[52])
-    # print(reversedButt[52])
-    # print( vincenty(reversedButt[52],reversedButt[4]).nautical )
-
-    # print( getDistance(reversedButt[52],reversedButt[4] ) )
 
     allAirports = allWisconsinAirportsList()
-
-    # point = (45.0,-89.0)
-    # point2 = allAirports[3]
-
-    # print( getDistance(point,point2))
-
-    # print( min([getDistance(point, eachAirport) for eachAirport in allAirports] ) )
 
     print( max( [(eachpoint, distanceToNearestAirport(eachpoint, allAirports) ) for eachpoint in tqdm( allWisconsinPoints(), total=600 ) ], key=lambda item: item[1] ) ) 
 
