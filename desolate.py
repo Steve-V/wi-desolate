75d4642e82171948cd089df1bbb6716f98b13937
Steve Vaught <Steven.D.Vaught@gmail.com>
2018-10-06 00:37:21 -0500

    cleanup some stale comments

--- desolate.py
+++ desolate.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python
 
-#import os
 from geopy.distance import great_circle
-from tqdm import tqdm
 from shapely.geometry import Polygon, Point
-import numpy, itertools, pickle
+import numpy, itertools
 
 def buildGrid():
     '''Return a grid of all latitude and longitude coordinates in the state, at whatever resolution. One of these will be our solution point.
     '''
-
-    # Step by a full degree for a coarse grid or a tenth of a degree for fine
-    #latstep = 1.0
-    #lonstep = -1.0
-    latstep = 0.1
-    lonstep = -0.1
 
     # Use shapely to build a polygon representing the land area of the state
     # For states like Colorado this is trivial but for non-square states we would import the coordinate list from a separate file
@@ -27,6 +19,9 @@ def buildGrid():
     borders = Polygon( stateBorders )
 
     # This represents the bounding latitude and longitude of the state. With a state like Colorado this approximates the actual borders, but with a state such as Florida this would also contain much ocean.
+    # Step by a full degree for a coarse grid or less (such as a tenth of a degree) for a finer solution
+    latstep = 1.0
+    lonstep = -1.0
     latitudeBox = numpy.arange(35,43,latstep)
     longitudeBox = numpy.arange(-101,-110,lonstep)
 
@@ -38,14 +33,14 @@ def distanceToNearestAirport(point, allAirports):
     Point is a tuple of the following format: (latitude, longitude)
     allAirports is an OrderedDict containing at least value['lat'] and value['long']
     See here for more on OrderedDicts: https://docs.python.org/3/library/csv.html
-    great_circle is provided by geopy, nautical is unnecessary but nice for verification'''
+    great_circle is provided by geopy, nautical is to make the answer easy to verify using standard aeronautical charts'''
     return min( [great_circle(point, (eachAirport['lat'],eachAirport['long'])).nautical for eachAirport in allAirports] )
 
 def main():
 
     '''Figure out the place in a state that is furthest from the nearest airport'''
 
-    # Airport data is stored on disk as a pickled OrderedDict in the working directory
+    # Airport data would be stored on disk as a pickled OrderedDict in the working directory. This is a small subset.
     allAirports = [
         {'long': -104.8493156, 'lat': 39.57013424, 'code': 'APA'},
         {'long': -104.6731767, 'lat': 39.86167312, 'code': 'DEN'},
@@ -53,15 +48,14 @@ def main():
         {'long': -105.1172046, 'lat': 39.90881199, 'code': 'BJC'}
     ]
 
-    # this list comprehension walks through allWisconsinPoints and returns a list of tuples: (the distance from a point to the nearest airport, and the point itself)
-    # the max function pulls out only the highest-distance value from the list of tuples
+    # this list comprehension walks through the grid of points and creates a list of tuples: (the distance from a provided point to the nearest airport, and the point itself)
+    # the max function takes the highest-distance value from the list of tuples, which is our answer
 
     howfar, furthestpoint = (max(
         ((distanceToNearestAirport(eachpoint, allAirports), eachpoint )
         for eachpoint in buildGrid() ) ) ) 
 
-    #Since i want to know which airport is nearest to the point, we run our known furthest point back through the distance algorithm again. 
-    # This listcomp takes our known furthest point and runs the calculation once more to find the nearest airport.
+    #Since i also want to know which airport is nearest to the point, we run our known furthest point back through the distance algorithm again. 
     # It outputs a tuple of the following format: (distance, airportname)
     result = min( [
         (great_circle(furthestpoint, (eachAirport['lat'], eachAirport['long']) ).nautical, eachAirport['code'])
